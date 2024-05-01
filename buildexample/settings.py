1cbc6a9b21d74fd35eb788860a457dc91dd019e8
Daniel Farmer <gas2me@virginia.edu>
2024-05-01 16:19:10 -0400

    taking out sensitive info

--- buildexample/settings.py
+++ buildexample/settings.py
@@ -58,8 +58,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'a03.host@gmail.com' 
-EMAIL_HOST_PASSWORD = 'knpztnotxcfrgzkq'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 
 
 # Application definition
